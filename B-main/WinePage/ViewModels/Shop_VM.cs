3fef981e863499c1a3e89498e2e34e6e88c58cdc
Anar <111847080+Anarqurbanov14@users.noreply.github.com>
2023-07-23 13:12:01 +0400

    test
    finish

---
+++ B-main/WinePage/ViewModels/Shop_VM.cs
@@ -0,0 +1,12 @@
+﻿using WinePage.Models;
+
+namespace WinePage.ViewModels;
+
+public class Shop_VM
+{
+    public List<Brand> Brands { get; set; }
+    public List<Category> Categories { get; set; }
+    public List<Property> Properties { get; set; }
+    public List<Color> Colors { get; set; }
+    public List<Models.Wine> Wine { get; set; }
+}
